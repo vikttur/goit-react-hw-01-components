e8ced973143e417b01d77f776c416216b274fb0c
Viktor Biruk <vikttur.job@gmail.com>
2023-05-29 15:17:22 +0300

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -2,8 +2,8 @@ export default function FriendListItem(props) {
 	const { id, avatar, name, isOnline } = props;
 
 	return (
-		<li className="item">
-			<span className="status">555{isOnline}</span>
+		<li className={isOnline ? "item" : ""}>
+			<span className="status">555</span>
 			<img className="avatar" src={avatar} alt="User avatar" width="48" />
 			<p className="name">{name}</p>
 		</li>
