156ba4e505202ce05c13d65165b464efedc66fc0
Viktor Biruk <vikttur.job@gmail.com>
2023-05-29 17:46:48 +0300

    render all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-// import user from "./data/user.json";
-// import data from "./data/data.json";
-// import friends from "./data/friends.json";
+import user from "./data/user.json";
+import data from "./data/data.json";
+import friends from "./data/friends.json";
 import transactions from "./data/transactions.json";
 
-// import Profile from "./components/Profile/Profile";
-// import Statistics from "./components/Statistics/Statistics";
-// import FriendList from "./components/FriendList/FriendList";
+import Profile from "./components/Profile/Profile";
+import Statistics from "./components/Statistics/Statistics";
+import FriendList from "./components/FriendList/FriendList";
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
-// const { username, tag, location, avatar, stats } = user;
+const { username, tag, location, avatar, stats } = user;
 
 export default function App() {
 	return (
-		// <Profile username={username} tag={tag} location={location} avatar={avatar} stats={stats} />
-		// <Statistics title={"Upload stats"} stats={data} />
-		// <FriendList friends={friends} />
-		<TransactionHistory items={transactions} />
+		<>
+			<Profile username={username} tag={tag} location={location} avatar={avatar} stats={stats} />
+			<Statistics title={"Upload stats"} stats={data} />
+			<FriendList friends={friends} />
+			<TransactionHistory items={transactions} />
+		</>
 	);
 }
